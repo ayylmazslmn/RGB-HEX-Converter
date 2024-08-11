64c325afc8edc58e3f65067c662c3257731a8356
Süleyman Ayyılmaz <101000802+ayylmazslmn@users.noreply.github.com>
2024-08-11 17:49:20 +0300

    Initial Commit

---
+++ RGB-HEX Converter/RGB_HEX_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGB_HEX_ConverterApp.swift
+//  RGB-HEX Converter
+//
+//  Created by Süleyman Ayyılmaz on 11.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct RGB_HEX_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
